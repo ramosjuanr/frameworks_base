c97d621b1433053f5ae2b4c7815a0253d9b3c5d9
Stallix <team.evolutionx.rom@gmail.com>
2020-01-02 16:45:46 +0100

    overlays: Add PornHub accent color overlay
    Change-Id: I9c1275dfbda74b91499379a7e8adb56241b515a1

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -25,6 +25,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorGreenOverlay \
 	AccentColorPurpleOverlay \
 	AccentColorRedOverlay \
+	AccentColorPornhubOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
 	DisplayCutoutEmulationTallOverlay \
