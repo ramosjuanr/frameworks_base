fdb311406fc3c7187d124d431f0fa9cb130ed350
Juan <jramos69@gmail.com>
2020-02-15 21:42:42 -0500

    Fix Phonewindowmanager
    Change-Id: Id47b15f29171cb53ffdd7378edd787e35030f471

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -208,6 +208,7 @@ import com.android.internal.policy.IShortcutService;
 import com.android.internal.policy.PhoneWindow;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.du.Utils;
 import com.android.server.ExtconStateObserver;
 import com.android.server.ExtconUEventObserver;
 import com.android.server.GestureLauncherService;
